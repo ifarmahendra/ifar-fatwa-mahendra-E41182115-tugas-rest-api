2539fdedb1b9130f2a41a94a30bb7210ed97a721
ifarmahendra <55471201+ifarmahendra@users.noreply.github.com>
2020-03-08 00:40:09 +0700

    tugas_restapi

---
+++ rest_ci/application/controllers/kontak.php
@@ -0,0 +1,66 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Kontak extends REST_Controller {
+    //pada syntax dibawah ini berfungsi untuk menampilkan database 
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+    //syntax dibawah berfungsi untuk menampilkan database berdasarkan id dan telepon ditabase
+    function index_get() {
+        $id = $this->get('id');
+        if ($id == '') {
+            $kontak = $this->db->get('telepon')->result();
+        } else {
+            $this->db->where('id', $id);
+            $kontak = $this->db->get('telepon')->result();
+        }
+        $this->response($kontak, 200);
+    }
+    //kode syntax dibawah ini digunakan untuk mengirim dan menambahkan data di database  
+    function index_post() {
+        $data = array(
+                    'id'           => $this->post('id'),
+                    'nama'          => $this->post('nama'),
+                    'nomor'    => $this->post('nomor'));
+        $insert = $this->db->insert('telepon', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+    //pada syntax dibawah ini digunakan untuk memperbarui data yang ada dalam database 
+    function index_put() {
+        $id = $this->put('id');
+        $data = array(
+                    'id'       => $this->put('id'),
+                    'nama'          => $this->put('nama'),
+                    'nomor'    => $this->put('nomor'));
+        $this->db->where('id', $id);
+        $update = $this->db->update('telepon', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+    //pada syntax dibawah ini digunakan untuk menghapus salah satu data yang telah tersimpan di dalam database
+    function index_delete() {
+        $id = $this->delete('id');
+        $this->db->where('id', $id);
+        $delete = $this->db->delete('telepon');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+}
+?>
